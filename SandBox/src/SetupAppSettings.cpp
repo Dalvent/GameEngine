96bd9ea75be56ac97eb391aba1fad07e6f13bbcb
Dmitrii <drozdovdm27@gmail.com>
2020-09-03 23:03:00 +0300

    gitignore

--- SandBox/src/SetupAppSettings.cpp
+++ SandBox/src/SetupAppSettings.cpp
@@ -7,7 +7,7 @@
 
 void IronCat::SetupAppSettings(IronCat::GameApplication& app)
 {
-	auto* gameMode = new SandBox();
+	auto* gameMode = new SandBox(); 
 	auto* colorLayer = new ColorChooseLayer();
 	app.SetGameMod(gameMode);
 	app.AddLayer(colorLayer);
